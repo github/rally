b74c6e09c32e95869c5863e01b51d4d0a4b00c13
Jared Murrell <primetheus@github.com>
2020-03-06 10:35:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ module.exports = app => {
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
 }
+
+var rally = require('rally'),
+var restApi = rally({
+        //user: 'userName', //required if no api key, defaults to process.env.RALLY_USERNAME
+        //pass: 'password', //required if no api key, defaults to process.env.RALLY_PASSWORD
+        //apiKey: '_12fj83fjk...', //preferred, required if no user/pass, defaults to process.env.RALLY_API_KEY
+        apiVersion: 'v2.0', //this is the default and may be omitted
+        server: 'https://rally1.rallydev.com',  //this is the default and may be omitted
+        requestOptions: {
+            headers: {
+                'X-RallyIntegrationName': 'Probot-Rally',    //while optional, it is good practice to
+                'X-RallyIntegrationVendor': 'GitHub, Inc',   //provide this header information
+                'X-RallyIntegrationVersion': '1.0'
+            }
+            //any additional request options (proxy options, timeouts, etc.)
+        }
+    });
