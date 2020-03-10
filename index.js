d124fa11781519ef9c32ac6c26fc046375221a80
Michael Johnson <migarjo@github.com>
2020-03-09 23:34:53 -0600

    Initial implementation ported from Jira Validator

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
+const RallyValidate = require('./lib/RallyValidate')
+
 /**
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
+  const handler = new RallyValidate(app)
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  app.on(['pull_request.opened', 'pull_request.labeled',
+    'pull_request.unlabeled', 'pull_request.edited',
+    'pull_request.reopened', 'pull_request.ready_for_review'],
+  async context => handler.handlePullRequest(context))
 }
-
-var rally = require('rally'),
-var restApi = rally({
-        //user: 'userName', //required if no api key, defaults to process.env.RALLY_USERNAME
-        //pass: 'password', //required if no api key, defaults to process.env.RALLY_PASSWORD
-        //apiKey: '_12fj83fjk...', //preferred, required if no user/pass, defaults to process.env.RALLY_API_KEY
-        apiVersion: 'v2.0', //this is the default and may be omitted
-        server: 'https://rally1.rallydev.com',  //this is the default and may be omitted
-        requestOptions: {
-            headers: {
-                'X-RallyIntegrationName': 'Probot-Rally',    //while optional, it is good practice to
-                'X-RallyIntegrationVendor': 'GitHub, Inc',   //provide this header information
-                'X-RallyIntegrationVersion': '1.0'
-            }
-            //any additional request options (proxy options, timeouts, etc.)
-        }
-    });
